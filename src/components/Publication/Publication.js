4e7532d7912c013333ea287cf5ff6491b1a87287
Alla Sariieva <alla.cleo@gmail.com>
2019-11-06 14:41:28 +0200

    hw done

---
+++ src/components/Publication/Publication.js
@@ -0,0 +1,21 @@
+/*eslint-disable*/
+
+import React from 'react';
+import propTypes from 'prop-types';
+import style from '../Reader.module.css';
+
+const Publication = ({ publication }) => (
+  <article className={style.publication}>
+    <h2>{publication.title}</h2>
+    <p>{publication.text}</p>
+  </article>
+);
+
+Publication.propTypes = {
+  publication: propTypes.shape({
+    title: propTypes.string.isRequired,
+    text: propTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Publication;
---
+++ src/components/Reader.js
@@ -0,0 +1,56 @@
+/*eslint-disable */
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import style from './Reader.module.css';
+
+import Controls from './Controls/Controls';
+import Counter from './Counter/Counter';
+import Publication from './Publication/Publication';
+
+export default class Reader extends Component {
+  static propTypes = {
+    publicationIdx: propTypes.number,
+    items: propTypes.arrayOf(propTypes.object.isRequired).isRequired,
+  };
+
+  static defaultProps = {
+    publicationIdx: 0,
+  };
+
+  state = {
+    publicationIdx: this.props.publicationIdx,
+  };
+
+  handleDecrement = () => {
+    this.setState(prevState => ({
+      publicationIdx: prevState.publicationIdx - 1,
+    }));
+  };
+
+  handleIncrement = () => {
+    this.setState(prevState => ({
+      publicationIdx: prevState.publicationIdx + 1,
+    }));
+  };
+
+  render() {
+    const { publicationIdx } = this.state;
+    const { items } = this.props;
+
+    return (
+      <div className={style.reader}>
+        <Controls
+          publicationIdx={publicationIdx}
+          itemsLength={items.length}
+          onDecrement={this.handleDecrement}
+          onIncrement={this.handleIncrement}
+        />
+        <Counter
+          publicationIdx={publicationIdx + 1}
+          itemsLength={items.length}
+        />
+        <Publication publication={items[publicationIdx]} />
+      </div>
+    );
+  }
+}
